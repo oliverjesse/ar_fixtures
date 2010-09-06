ce123a429bb63740215cb40bbd417ecdaf4db105
Jesse Oliver Sanford <jos7@columbia.edu>
2010-09-06 13:32:55 -0700

    modified to use json and some other stuff

--- lib/ar_fixtures.rb
+++ lib/ar_fixtures.rb
@@ -1,5 +1,3 @@
-require 'erb'
-
 # Extension to make it easy to read and write data to a file.
 class ActiveRecord::Base
 
@@ -8,66 +6,44 @@ class ActiveRecord::Base
     # Writes content of this table to db/table_name.yml, or the specified file.
     #
     # Writes all content by default, but can be limited.
-    def dump_to_file(path=nil, limit=nil, opts={})
-      opts[:limit] = limit if limit
-      path ||= "db/#{table_name}.yml"
-      write_file(File.expand_path(path, RAILS_ROOT), self.find(:all, opts).to_yaml)
-      habtm_to_file
+    def dump_to_file(path=nil, opts=[])
+      path ||= "db/#{table_name}.json"
+      records = case opts
+        when Array
+          self.find(opts)
+        when Hash
+          self.find(:all, opts)
+      end.to_json
+      write_file(File.expand_path(path, RAILS_ROOT), records)
     end
-
-    # dump the habtm association table
-    def habtm_to_file
-      path ||= "db/#{table_name}.yml"
-      joins = self.reflect_on_all_associations.select { |j|
-        j.macro == :has_and_belongs_to_many
-      }
-      joins.each do |join|
-        hsh = {}
-        connection.select_all("SELECT * FROM #{join.options[:join_table]}").each_with_index { |record, i|
-          hsh["join_#{'%05i' % i}"] = record
-        }
-        write_file(File.expand_path("db/#{join.options[:join_table]}.yml", RAILS_ROOT), hsh.to_yaml(:SortKeys => true))
-      end
-    end
-
-    ##
+  
     # Delete existing data in database and load fresh from file in db/table_name.yml
-    #
-    # ERB tags are also possible, as with the rest of Rails fixtures.
     def load_from_file(path=nil)
-      path ||= "db/#{table_name}.yml"
+      path ||= "db/#{table_name}.json"
 
       self.destroy_all
 
       if connection.respond_to?(:reset_pk_sequence!)
-        connection.reset_pk_sequence!(table_name)
+       connection.reset_pk_sequence!(table_name)
       end
 
-      rawdata = File.read(File.expand_path(path, RAILS_ROOT))
-      erb_data = ERB.new(rawdata).result
-      records = YAML::load( erb_data )
-
+      records = JSON::parse( File.open( File.expand_path(path, RAILS_ROOT), 'rb' ) { |f| f.read })
       records.each do |record|
-        unless 'test' == RAILS_ENV
-          puts "______________"
-          puts record.to_yaml
-          puts "______________"
-        end
-        
-        record_copy = self.new(record.attributes)
-        record_copy.id = record.id
+        attributes = record[table_name.singularize.to_s]
+        record_copy = self.new(attributes)
+        record_copy.id = attributes['id']
 
         # For Single Table Inheritance
-        klass_col = record.class.inheritance_column.to_sym
-        if record[klass_col]
-          record_copy.type = record[klass_col]
+        klass_col = self.inheritance_column
+        if attributes[klass_col]
+           record_copy.type = attributes[klass_col]
         end
-
+      
         record_copy.save
       end
-
+ 
       if connection.respond_to?(:reset_pk_sequence!)
-        connection.reset_pk_sequence!(table_name)
+       connection.reset_pk_sequence!(table_name)
       end
     end
 
@@ -75,13 +51,14 @@ class ActiveRecord::Base
     # Uses existing data in the database.
     #
     # Will be written to +test/fixtures/table_name.yml+. Can be restricted to some number of rows.
-    def to_fixture(limit=nil, key=nil, opts={})
+    def to_fixture(limit=nil)
+      opts = {}
       opts[:limit] = limit if limit
 
-      write_file(File.expand_path("test/fixtures/#{table_name}.yml", RAILS_ROOT),
-      self.find(:all, opts).inject({}) { |hsh, record|
-        hsh.merge((record.attributes[key.to_s] || "#{table_name.singularize}_#{'%05i' % record.id rescue record.id}") => record.attributes)
-      }.to_yaml(:SortKeys => true))
+      write_file(File.expand_path("test/fixtures/#{table_name}.json", RAILS_ROOT), 
+          self.find(:all, opts).inject({}) { |hsh, record| 
+              hsh.merge("#{table_name.singularize}_#{'%05i' % record.id}" => record.attributes) 
+            }.to_yaml(:SortKeys => true))
       habtm_to_fixture
     end
 
@@ -95,10 +72,10 @@ class ActiveRecord::Base
         connection.select_all("SELECT * FROM #{join.options[:join_table]}").each_with_index { |record, i|
           hsh["join_#{'%05i' % i}"] = record
         }
-        write_file(File.expand_path("test/fixtures/#{join.options[:join_table]}.yml", RAILS_ROOT), hsh.to_yaml(:SortKeys => true))
+        write_file(File.expand_path("test/fixtures/#{join.options[:join_table]}.json", RAILS_ROOT), hsh.to_yaml(:SortKeys => true))
       end
     end
-
+    
     # Generates a basic fixture file in test/fixtures that lists the table's field names.
     #
     # You can use it as a starting point for your own fixtures.
@@ -112,12 +89,12 @@ class ActiveRecord::Base
     #
     # TODO Automatically add :id field if there is one.
     def to_skeleton
-      record = {
-        "record_1" => self.new.attributes,
-        "record_2" => self.new.attributes
-      }
-      write_file(File.expand_path("test/fixtures/#{table_name}.yml", RAILS_ROOT),
-      record.to_yaml)
+      record = { 
+          "record_1" => self.new.attributes,
+          "record_2" => self.new.attributes
+         }
+      write_file(File.expand_path("test/fixtures/#{table_name}.json", RAILS_ROOT),
+        record.to_yaml)
     end
 
     def write_file(path, content) # :nodoc:
